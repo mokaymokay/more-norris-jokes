a80fa46726661860c21f9f0f5e747833c1c7ed9b
Kay Mok <kaymok3@gmail.com>
2018-04-12 20:26:39 -0400

    Add route to get random joke

--- server.rb
+++ server.rb
@@ -3,7 +3,7 @@ require 'sinatra/reloader'
 require 'chuck_norris'
 
 get '/' do
-  "hello world"
+  'hello world'
 end
 
 get '/:type' do
@@ -13,5 +13,21 @@ get '/:type' do
   elsif params[:type] == "nsfw"
     @nsfw_joke = ChuckNorris::JokeFinder.find_joke('random?limitTo=[explicit]').joke
     erb :nsfw
+  elsif params[:type] == "surprise"
+    random = rand(10)
+    if random > 4
+      @random_joke = ChuckNorris::JokeFinder.find_joke('random?limitTo=[nerdy]').joke
+      erb :surprise
+    else
+      @random_joke = ChuckNorris::JokeFinder.find_joke('random?limitTo=[explicit]').joke
+      erb :surprise
+    end
+  else
+    'this page does not exist'
   end
 end
+
+# get '/surprise' do
+#   @random_joke = ChuckNorris::JokeFinder.get_joke.joke
+#   erb :surprise
+# end
